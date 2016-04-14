9584f56544ee0996ac8e43ec22afc1725fa7f2ac
Ira Rosen <IRAR@il.ibm.com>
2016-04-14 14:33:05 +0300

    IBM-Swift/Kitura#343 Session authentication with Credentials

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Kitura-Credentials-Sample
 A sample web application for authentication using Kitura-Credentials
+
+![Mac OS X](https://img.shields.io/badge/os-Mac%20OS%20X-green.svg?style=flat)
+![Linux](https://img.shields.io/badge/os-linux-green.svg?style=flat)
+![Apache 2](https://img.shields.io/badge/license-Apache2-blue.svg?style=flat)
+
+## Summary
+
+This is a sample [Kitura](https://github.com/IBM-Swift/Kitura) application for authentication using [Kitura-Credentials](https://github.com/IBM-Swift/Kitura-Credentials). See instructions for [Installation on OS X ](https://github.com/IBM-Swift/Kitura#installation-os-x) or [Installation on Linux](https://github.com/IBM-Swift/Kitura#installation-linux-apt-based).
+
+## Build and run
+1. `make run`
+
+  You should see message _Listening on port 8090_. The result executable is located in `.build/debug` directory: `./.build/debug/KituraSample`
+2. Open your browser at [http://localhost:8090/private](http://localhost:8090/private)
+
+## License
+
+This sample app is licensed under Apache 2.0. Full license text is available in [LICENSE](LICENSE.txt).
--- Sources/KituraCredentialsSample/main.swift
+++ Sources/KituraCredentialsSample/main.swift
@@ -14,12 +14,15 @@
  * limitations under the License.
  **/
 
-// KituraCredentialsSample shows examples for authentication using Kitura-Credentials framework.
+// KituraSample shows examples for creating custom routes.
 
 import KituraSys
 import KituraNet
 import Kitura
-
+import KituraSession
+import Credentials
+import CredentialsFacebook
+import CredentialsGoogle
 import LoggerAPI
 import HeliumLogger
 
@@ -38,16 +41,110 @@ let router = Router()
 // Using an implementation for a Logger
 Log.logger = HeliumLogger()
 
-router.get("/hello") { _, response, next in
+router.all(middleware: Session(secret: "Very very secret....."))
+
+
+// Authentication
+
+let credentials = Credentials()
+
+let fbClientId = "Facebook client ID"
+let fbCallbackUrl = "serverUrl" + "/login/facebook/callback"
+let fbClientSecret = "Facebook client secret"
+let googleClientId = "Google client ID"
+let googleCallbackUrl = "serverUrl" + "/login/google/callback"
+let googleClientSecret = "Google client secret"
+
+let fbCredentials = CredentialsFacebook(clientId: fbClientId, clientSecret: fbClientSecret, callbackUrl: fbCallbackUrl)
+let googleCredentials = CredentialsGoogle(clientId: googleClientId, clientSecret: googleClientSecret, callbackUrl: googleCallbackUrl)
+credentials.register(fbCredentials)
+credentials.register(googleCredentials)
+
+credentials.options["failureRedirect"] = "/login"
+credentials.options["successRedirect"] = "/private/data"
+
+router.all("/private", middleware: credentials)
+router.get("/private/data", handler:
+    { request, response, next in
+        print("/private/data")
+        response.setHeader("Content-Type", value: "text/html; charset=utf-8")
+        do {
+            if let userProfile = request.userProfile  {
+                try response.status(HttpStatusCode.OK).send(
+                    "<!DOCTYPE html><html><body>" +
+                        "Hello " +  userProfile.displayName + "! You are logged in with " + userProfile.provider + ". This is private!</body></html>\n\n").end()
+                next()
+                return
+            }
+            try response.status(HttpStatusCode.OK).send(
+                "<!DOCTYPE html><html><body>" +
+                    "Welcome! Please <a href=/login>login</a>" +
+                "</body></html>\n\n").end()
+        }
+        catch {}
+        next()
+})
+
+router.get("/login") { request, response, next in
+    print("/login")
+    response.setHeader("Content-Type", value: "text/html; charset=utf-8")
     do {
-        try response.send("Hello, World!").end()
+        try response.status(HttpStatusCode.OK).send(
+            "<!DOCTYPE html><html><body>" +
+                "<a href=/login/facebook>Log In with Facebook</a><br>" +
+                "<a href=/login/google>Log In with Google</a>" +
+            "</body></html>\n\n").end()
     }
-    catch{}
+    catch {}
+    next()
+}
+
+router.get("/login/facebook",
+           handler: credentials.authenticate(fbCredentials.name))
+router.get("/login/google",
+           handler: credentials.authenticate(googleCredentials.name))
+router.get("/login/facebook/callback",
+           handler: credentials.authenticate(fbCredentials.name, failureRedirect: "/login"))
+router.get("/login/google/callback",
+           handler: credentials.authenticate(googleCredentials.name, failureRedirect: "/login"))
+
+
+
+// Handles any errors that get set
+router.error { request, response, next in
+    response.setHeader("Content-Type", value: "text/plain; charset=utf-8")
+    do {
+        let errorDescription: String
+        if let error = response.error {
+            errorDescription = "\(error)"
+        } else {
+            errorDescription = "Unknown error"
+        }
+        try response.send("Caught the error: \(errorDescription)").end()
+    }
+    catch {
+        Log.error("Failed to send response \(error)")
+    }
+}
+
+// A custom Not found handler
+router.all { request, response, next in
+    if  response.getStatusCode() == .NOT_FOUND  {
+        // Remove this wrapping if statement, if you want to handle requests to / as well
+        if  request.originalUrl != "/"  &&  request.originalUrl != ""  {
+            do {
+                try response.send("Route not found in Sample application!").end()
+            }
+            catch {
+                Log.error("Failed to send response \(error)")
+            }
+        }
+    }
     next()
 }
 
 // Listen on port 8090
 let server = HttpServer.listen(8090,
-    delegate: router)
+                               delegate: router)
 
 Server.run()
