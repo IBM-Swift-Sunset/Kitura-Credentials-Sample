3512ced5b471ab877428ba6ba7b1255f28a53a4e
Shmuel Kallner <kallner@il.ibm.com>
2016-09-08 16:33:10 +0300

    updated dependency versions in Package.swift
    set version of https://github.com/IBM-Swift/Kitura-CredentialsFacebook.git to 0.30.0
    set version of https://github.com/IBM-Swift/Kitura-CredentialsGoogle.git to 0.30.0
    set version of https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git to 0.30.0
    set version of https://github.com/IBM-Swift/HeliumLogger.git to 0.16.0

--- Package.swift
+++ Package.swift
@@ -25,10 +25,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsFacebook.git", majorVersion: 0, minor: 28),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsGoogle.git", majorVersion: 0, minor: 28),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git", majorVersion: 0, minor: 28),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 15),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsFacebook.git", majorVersion: 0, minor: 30),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsGoogle.git", majorVersion: 0, minor: 30),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsHTTP.git", majorVersion: 0, minor: 30),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 16),
     ],
     exclude: ["Makefile", "Package-Builder"])
 
