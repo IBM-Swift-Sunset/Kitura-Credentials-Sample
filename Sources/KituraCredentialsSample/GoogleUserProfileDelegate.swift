b921bd7ff057df5f3b09f1d43e22759555b39bdd
Ira Rosen <IRAR@il.ibm.com>
2016-12-04 10:46:14 +0200

    IBM-Swift/Kitura#888 Update GoogleUserProfileDelegate

--- Sources/KituraCredentialsSample/GoogleUserProfileDelegate.swift
+++ Sources/KituraCredentialsSample/GoogleUserProfileDelegate.swift
@@ -17,35 +17,12 @@
 import Credentials
 
 public class GoogleUserProfileDelegate: UserProfileDelegate {
-    public func userProfileToDictionary(_ userProfile: UserProfile) -> [String:Any] {
-        var dictionary = [String:String]()
-        dictionary["displayName"] = userProfile.displayName
-        dictionary["id"] = userProfile.id
-        dictionary["provider"] = userProfile.provider
-        dictionary["email"] = userProfile.emails?[0].value
-        dictionary["gender"] = userProfile.extendedProperties?["gender"] as? String
-        return dictionary
-    }
-    
-    public func identityProviderDictionaryToUserProfile(_ dictionary: [String:Any]) -> UserProfile? {
-        if let name = dictionary["name"] as? String,
-            let id = dictionary["sub"] as? String,
-            let email = dictionary["email"] as? String,
-            let gender = dictionary["gender"] as? String {
-            let userEmail = UserProfile.UserProfileEmail(value: email, type: "")
-            return UserProfile(id: id, displayName: name, provider: "Google", emails: [userEmail], extendedProperties: ["gender":gender])
+    public func update(userProfile: UserProfile, from dictionary: [String:Any]) {
+        if let gender = dictionary["gender"] as? String {
+            if userProfile.extendedProperties == nil {
+                userProfile.extendedProperties = [String:Any]()
+            }
+            userProfile.extendedProperties!["gender"] = gender
         }
-        return nil
-    }
-    
-    public func dictionaryToUserProfile(_ dictionary: [String:Any]) -> UserProfile? {
-        if let name = dictionary["displayName"] as? String,
-            let id = dictionary["id"] as? String,
-            let email = dictionary["email"] as? String,
-            let gender = dictionary["gender"] as? String {
-            let userEmail = UserProfile.UserProfileEmail(value: email, type: "")
-            return UserProfile(id: id, displayName: name, provider: "Google", emails: [userEmail], extendedProperties: ["gender":gender])
-        }
-        return nil
     }
 }
